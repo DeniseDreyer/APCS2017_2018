3d4e57fd48057da379bc4375a4515235d78da576
DeniseDreyer <ddreyer@millshighsch.org>
2017-08-24 09:27:42 -0700

    Third time's a charm :-)

---
+++ Project2B/src/Project2B.java
@@ -0,0 +1,17 @@
+//Denise Dreyer
+/* August 24, 2017
+This clss compares output using print and println
+*/
+
+public class Project2B {
+
+	public static void main(String[] args) {
+		System.out.print("Hello");
+		System.out.println("Hello 2");
+		System.out.println("Hello 3");
+
+
+
+	}
+
+}
