a1e005a9b5758445fdb28c8bf1b7b7aa1e93f398
DeniseDreyer <ddreyer@millshighsch.org>
2017-09-05 10:50:43 -0700

    Restarting the commit & sync

--- Project2/src/Project2.java
+++ Project2/src/Project2.java
@@ -7,7 +7,6 @@ public class Project2 {
 
 	public static void main(String[] args) {
 		System.out.println("hello -- this is project 2!!!");
-		
 		System.out.print("hello -- this is project 2!!!");
 	
 	}
