aeb4b1a3f1d5c381c295e60d3a1fbf377893b006
DeniseDreyer <ddreyer@millshighsch.org>
2017-09-06 11:07:32 -0700

    Calculate Library with DoMath.  Square is complete

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,12 @@
+/*Denise Dreyer
+ * September 6, 2017
+ * This is the runner for the calculate library.  It is used to test the methods in Calculate. *  
+ */
+public class DoMath {
+
+	public static void main(String[] args) {
+		System.out.println(Calculate.square(-7));
+
+	}
+
+}
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,13 @@
+/*Denise Dreyer
+ * September 6, 2017
+ * Our self written math library
+ * It contains a series of methods to do basic math functions. 
+ */
+public class Calculate {
+
+	//square the input
+	public static int square (int operand) {
+		int squared =operand*operand;
+		return squared;
+	}
+}
