7ce0be0b679f8cd93acb0fa815e62c05b14a1533
DeniseDreyer <ddreyer@millshighsch.org>
2017-10-27 09:50:26 -0700

    split part 0 &1

--- Split String/src/Split.java
+++ Split String/src/Split.java
@@ -18,31 +18,64 @@ public class Split
 		//		it will split at the word "really" and return an array of ["I "," like "," apples!"]
 		
 		//play around with String.split! what happens if you "I reallyreally like apples".split("really") ?
-	//	String[] arr="I like apples!".split(" ");
-		System.out.println(Arrays.toString("I like apples!".split(" ")));
-			
-		System.out.println(Arrays.toString("I really like really red apples".split("really")));
-		System.out.println(Arrays.toString("I really like reallyreally red apples".split("really")));
-		System.out.println(Arrays.toString("really like really red apples".split("really")));
-		System.out.println(Arrays.toString("I really like really red apples,really".split("really")));
+		String[] arr="I like apples!".split(" ");  //putting the result from split in an array
+		System.out.println(Arrays.toString(arr));  //using toString to print the array
 		
-		//Your task:
+		System.out.println(Arrays.toString("I like apples!".split(" ")));  //Example 1
+		System.out.println(Arrays.toString("I really like really red apples".split("really")));  //Example 2
+		System.out.println(Arrays.toString("I really like reallyreally red apples".split("really"))); //testing 2 "really"s together
+		System.out.println(Arrays.toString("really like really red apples".split("really"))); // testing a "really" at the beginning
+		System.out.println(Arrays.toString("I really like really red apples,really".split("really"))); //testing "really" at the end
+	
+	
+	
+	//Call code for part 1 and 2
+	
+		getTheFilling("applespineapplesbreadlettustomatobaconmayohambreadcheese");
+		getTheFilling("apples pineapples bread lettus tomato bacon mayo ham bread cheese");
+		getTheFilling("apples pineapples bread lettus tomato bacon mayo ham ");
+		getTheFilling("apples pineapples lettus tomato bacon mayo ham  ");
+		getTheFilling("bread");
+		getTheFilling("breadbread");
+		getTheFilling("applesbreadpineapplesbreadlettustomatobaconmayohambreadcheese");
+	
+	}
+	public static void getTheFilling(String everything) {
+		//Your task Part 1:
 		/*Write a method that take in a string like "applespineapplesbreadlettustomatobaconmayohambreadcheese" describing a sandwich
 		 * use String.split to split up the sandwich by the word "bread" and return what's in the middle of the sandwich and ignores what's on the outside
 		 * What if it's a fancy sandwich with multiple pieces of bread?
 		*/
+	
 		
-		System.out.println(Arrays.toString("applespineapplesbreadlettustomatobaconmayohambreadcheese".split("bread")));
+//		String[] parts ="breadlettustomatobaconmayohambreadcheese".split("bread");
+		String[] parts =everything.split("bread");
+		String filling="";
 		
+		//test to see if there are two pieces of bread
+		if (everything.indexOf("bread") <=0 ) {
+			filling = "\"" + everything + "\"" + "is not a sandwich";
+		}
+		else if (everything.substring(everything.indexOf("bread")).indexOf("bread")<0) {
+			filling = "\"" + everything + "\"" + "is not a sandwich";
+		}
+		else {
+			 filling ="\"" + everything + "\"" + parts[1];
+		}
+//		String[] parts ="applespineapplesbreadlettustomatobaconmayohambreadcheese".split("bread");
+		//System.out.println(Arrays.toString(parts));
+	
 		
+		System.out.println(filling);
+	}	
 		//Your task pt 2:
 		/*Write a method that take in a string like "apples pineapples bread lettus tomato bacon mayo ham bread cheese" describing a sandwich
 		 * use String.split to split up the sandwich at the spaces, " ", and return what's in the middle of the sandwich and ignores what's on the outside
 		 * Again, what if it's a fancy sandwich with multiple pieces of bread?
 		*/
-
+// added line -----
 		
 
 	}
 
-}
+
